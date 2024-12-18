91643681ef0777b8b4f2422b8ea6fb706a8ab8be
Franklin Kong <franklinkong981@gmail.com>
2024-12-18 11:35:09 -0800

    Imported starter code

---
+++ react-carousel-testing/src/photos.js
@@ -0,0 +1,20 @@
+import image1 from "./assets/image1.jpg";
+import image2 from "./assets/image2.jpg";
+import image3 from "./assets/image3.jpg";
+
+const photos = [
+  {
+    src: image1,
+    caption: "Photo by Richard Pasquarella on Unsplash"
+  },
+  {
+    src: image2,
+    caption: "Photo by Pratik Patel on Unsplash"
+  },
+  {
+    src: image3,
+    caption: "Photo by Josh Post on Unsplash"
+  }
+];
+
+export default photos;
